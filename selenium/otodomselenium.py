1ad9cbfbfd97fdbf17b5de76e195d2c69f76f748
knagiel <100476208+knagiel@users.noreply.github.com>
2022-05-14 19:46:15 +0200

    Update otodomselenium.py

--- selenium/otodomselenium.py
+++ selenium/otodomselenium.py
@@ -1,46 +1,52 @@
-from selenium import webdriver
-from selenium.common import exceptions
+# -*- coding: utf-8 -*-
 import pandas as pd
+import scrapy
+from selenium import webdriver
+from scrapy.selector import Selector
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
 
+name = 'dom'  # if you use base of spider but want to scrape other page, you need to update name of the spider
+allowed_domains = ['otodom.pl']
+start_urls = ['http://otodom.pl/']
 
 driver = webdriver.Chrome()
-url = 'https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/mazowieckie?page='
-driver.get(url)
+driver.get('https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/mazowieckie?page=1')
+sleep(3)
 
-infos = driver.find_elements(By.CLASS_NAME, 'css-14cy79a e3x1uf06')
-
-for info in infos:
-    title = info.find_elements(By.XPATH, './/*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/div[1]/h3').text
-    price = info.find_elements(By.XPATH, './/*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/div[2]/span[1]').text
-    location = info.find_elements(By.XPATH, './/*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/p/span').text
-    print(title, price, location)
+Button = driver.find_element(by=By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]')
+Button.click()
 
 title_list = []
 price_list = []
 location_list = []
 
-i = 0
-while i < 100:
-    try:
-        apartment_info = driver.find_element(By.CLASS_NAME, 'css-14cy79a e3x1uf06')
+def parse(self, response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    contents = soup.find_all('li', {'class': 'css-p74l73 es62z2j17'})
 
-        for info in infos:
-            title = info.find_elements(By.XPATH, './/*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/div[1]/h3').text
-            price = info.find_elements(By.XPATH, './/*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/div[2]/span[1]').text
-            location = info.find_elements(By.XPATH, './/*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/p/span').text
-            print(title, price, location)
 
-            title_list.append(title)
-            price_list.append(price)
-            location_list.append(location)
+    for content in contents:
+        title = content.find_element(By.XPATH,
+                                     '//*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/div[1]/h3').text
+        price = content.find_element(By.XPATH,
+                                     './/*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/div[2]/span[1]').text
+        location = content.find_element(By.XPATH,
+                                        './/*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[2]/div[2]/ul/li[1]/a/article/p/span').text
 
-        driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/main/div[1]/div[3]/div[1]/div[4]/div/nav/button[6]').click()
-        i += 1
-
-    except exceptions.StaleElementReferenceException:
-        pass
-
+        title_list.append(title)
+        price_list.append(price)
+        location_list.append(location)
+    
+    
+    for url in range(2, 101):
+        next_page = 'https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/mazowieckie?page=' + str(url)
+        next_page = response.urljoin(next_page)
+        yield scrapy.Request(next_page, callback=self.parse)
+        
 df = pd.DataFrame(list(zip(title_list, price_list, location_list)), columns=['name', 'location', 'price'])
 
 homes_for_sale = df.to_csv('mazowieckie_for_sale.csv', index=False)
